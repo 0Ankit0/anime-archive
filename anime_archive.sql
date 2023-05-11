792f3c22dbce4f25670045f707adfed487168c91
ankit <ankit@DESKTOP>
2023-05-11 16:02:36 +0545

    user table added

--- anime_archive.sql
+++ anime_archive.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 11, 2023 at 12:02 PM
+-- Generation Time: May 11, 2023 at 12:16 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 

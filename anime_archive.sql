7fa32e4969de6006aed0969aa2755b0300e1fabd
ankit <ankit@DESKTOP>
2023-05-11 15:47:40 +0545

    database changed

--- anime_archive.sql
+++ anime_archive.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 07, 2023 at 10:49 AM
+-- Generation Time: May 11, 2023 at 12:02 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
